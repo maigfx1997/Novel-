c515aca0320e215dcd47c693f22405d04e52dd13
maigfx1997 <maigfx1997@gmail.com>
2026-08-23 19:38:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, send_file, jsonify, render_template_string
 from flask_cors import CORS
 import requests
@@ -373,9 +374,10 @@ def convert_novel():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-import os
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
+
 
