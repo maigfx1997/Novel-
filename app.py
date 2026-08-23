f45fcfb83cc8047bb8b004ad282175652e714c0c
maigfx1997 <maigfx1997@gmail.com>
2026-08-23 19:35:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -373,5 +373,9 @@ def convert_novel():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+import os
+
 if __name__ == '__main__':
-    app.run(0.0.0.0, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
